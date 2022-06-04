b2e021bfc9f8c414776fee054b37851b02a118cf
RameshNeupane <neupaneramesh555@gmail.com>
2022-06-04 09:37:34 +0545

    remove console.log

--- assignments/design-final-assignment/js/script.js
+++ assignments/design-final-assignment/js/script.js
@@ -1,8 +1,6 @@
 const navBar = document.getElementsByClassName("nav")[0];
-console.log(navBar);
 
 const iconToggle = document.getElementsByClassName("icn__toggle")[0];
-console.log(iconToggle);
 
 iconToggle.addEventListener("click", () => {
   navBar.classList.toggle("active");
